93aac523dddfc934f21205cfbffaebe1b869abff
Brad Barrows <bebrws@gmail.com>
2020-09-02 18:52:42 -0700

    Improve the fuzzy string search by heavily weighting characters in a row and add better color formatting and clean up some code

--- fznim.nim
+++ fznim.nim
@@ -5,21 +5,18 @@ import strutils
 import os
 import posix
 
+# type
+#   ttysize* {.bycopy.} = object
+#     ts_lines*: cushort
+#     ts_cols*: cushort
+#     ts_xxx*: cushort
+#     ts_yyy*: cushort
+# var ts:ttysize
+# var r = ioctl(1, 1074295912, addr ts)
+# var w = int(ts.ts_cols)
 
-type
-  ttysize* {.bycopy.} = object
-    ts_lines*: cushort
-    ts_cols*: cushort
-    ts_xxx*: cushort
-    ts_yyy*: cushort
-
-
-var ts:ttysize
-var r = ioctl(1, 1074295912, addr ts)
-
-var w = int(ts.ts_cols)
+var w = terminalWidth()
 var h = terminalHeight()
-var maxWidthOfItem = w - 4
 
 proc fdopen(f: cint, mode: cstring): File {.
     importc: "fdopen", header: "<stdio.h>", tags: [].}
@@ -30,8 +27,8 @@ proc freopen(filename, mode: cstring, stream: File): File {.
 proc c_fileno(f: File): cint {.
     importc: "fileno", header: "<fcntl.h>".}
 
-proc printf(formatstr: cstring) {.importc: "printf", varargs,
-                                  header: "<stdio.h>".}
+# proc printf(formatstr: cstring) {.importc: "printf", varargs,
+#                                   header: "<stdio.h>".}
                                   
 
 proc fzfuzzyMatch*(pattern: string, str: string, longestItemLength: int) : tuple[score: int, matched: bool, highlighted: string] =
@@ -40,6 +37,7 @@ proc fzfuzzyMatch*(pattern: string, str: string, longestItemLength: int) : tuple
     patIndex = 0
     lastCharMatchedScore = 0
     score = 0
+    numInRow = 0
     highlightedString = ""
 
   while (strIndex < str.len) and (patIndex < pattern.len):
@@ -57,17 +55,19 @@ proc fzfuzzyMatch*(pattern: string, str: string, longestItemLength: int) : tuple
       continue
 
     if strIndex == 0 and patternChar == strChar:
-      highlightedString &= "\e[1;33m" & str[strIndex] & "\e[00m"
+      highlightedString &= "\e[1;39m" & str[strIndex] & "\e[00m"
       score += 3
       lastCharMatchedScore += 2
       patIndex += 1
       strIndex += 1
+      numInRow += 1
     elif strChar == patternChar:
-      highlightedString &= "\e[1;31m" & str[strIndex] & "\e[00m"
-      score += int(longestItemLength/strIndex)  # float(float(1) * float(patIndex))
+      highlightedString &= "\e[1;44m" & str[strIndex] & "\e[00m"
+      score += int(longestItemLength/strIndex) * (if numInRow == 0: 1 else: (numInRow * 3))
       if lastCharMatchedScore != 0:
         lastCharMatchedScore += 2
         score += lastCharMatchedScore
+      numInRow += 1
       strIndex += 1
       patIndex += 1
     else:
@@ -75,8 +75,9 @@ proc fzfuzzyMatch*(pattern: string, str: string, longestItemLength: int) : tuple
         lastCharMatchedScore = 0
       highlightedString &= str[strIndex]
       strIndex += 1
+      numInRow = 0
   
-  while strIndex < str.len:
+  while strIndex < str.len and strIndex < (w - 4):
     highlightedString &= str[strIndex]
     strIndex += 1
       
@@ -100,13 +101,19 @@ type
     item: string
     score: int
 
-var oldStartOfItemsToStartShowingFrom = -1
-var oldAnswer = ""
 var oldSelLocation = 1
 var matches: seq[ItemsMatch]
 var itemsToSearch: seq[ItemsMatch]
 
-proc fuzzySearchItems(answer: string, items: seq[string]): seq[tuple[index: int, item: string, score: int]] =
+proc colorEndOfString(str: string): string =
+  var loc = str.rfind("\e[00m")
+  var lastEndOfColor = if loc < 0: 0 else: loc
+  if str.len > 3 and lastEndOfColor < str.len - 6 and lastEndOfColor >= 0:
+    return str[0..(lastEndOfColor-1)] & "\e[1;39m" & str[lastEndOfColor+(if loc < 0: 0 else: 5)..str.len-1] & "\e[00m"
+  else:
+    return str
+
+proc fuzzySearchItems(sel: int, answer: string, items: seq[string]): seq[tuple[index: int, item: string, score: int]] =
   # Create itemsToSearch which is a list of tuples containing the original index of a search item,
   # the search item itself, and the match score of how well the search item matched against the 
   # prompt "answer" that is being typed by the user
@@ -127,7 +134,12 @@ proc fuzzySearchItems(answer: string, items: seq[string]): seq[tuple[index: int,
     itemsToSearch = matches
   else:
     for index, item in items:
-      itemsToSearch.add((index, $item, index))
+      itemsToSearch.add((index, item, index))
+
+  for index, (originalIndex, item, score) in itemsToSearch:
+    # newStr &= "\e[1;34m" & c & "\e[00m"
+    var str = if sel == index: "\e[1;38m- \e[00m" & colorEndOfString(item) else: item
+    itemsToSearch[index].item = str
   return itemsToSearch
 
 proc drawPromptItemsAndSelector(prompt: string, answer: string, itemsToSearch: seq[tuple[index: int, item: string, score: int]], sel: var int): int =
@@ -136,6 +148,18 @@ proc drawPromptItemsAndSelector(prompt: string, answer: string, itemsToSearch: s
   # The length of the list is the height of the window minus 1 line for the prompt and 2 lines for spacing at the bottom
   var shownListBottom = h - 1
   var shownListLength = shownListBottom - 2
+
+  # Get the location where the selector will be rendered
+  var selLocation = sel
+  if sel + 1 > shownListBottom - 2:
+    selLocation = shownListBottom - 2
+  else:
+    selLocation += 1
+
+  if oldSelLocation != selLocation:
+    setCursorPos(0, oldSelLocation)
+    echo " "
+    oldSelLocation = selLocation    
 
   var numberOfItemsToShowAfterStart = len(itemsToSearch) - 1
   var startOfItemsToStartShowingFrom = 0
@@ -159,26 +183,26 @@ proc drawPromptItemsAndSelector(prompt: string, answer: string, itemsToSearch: s
 
   var endOfItemsToShowTo = numberOfItemsToShowAfterStart + startOfItemsToStartShowingFrom
 
-  if oldStartOfItemsToStartShowingFrom != startOfItemsToStartShowingFrom or oldAnswer != answer:
-    eraseScreen()
-    oldStartOfItemsToStartShowingFrom = startOfItemsToStartShowingFrom
-    for index, val in itemsToSearch[startOfItemsToStartShowingFrom..endOfItemsToShowTo]:
-      setCursorPos(2, (index + 1))
+  #if oldStartOfItemsToStartShowingFrom != startOfItemsToStartShowingFrom or oldAnswer != answer:
+  eraseScreen()
+  # oldStartOfItemsToStartShowingFrom = startOfItemsToStartShowingFrom
+  for index, val in itemsToSearch[startOfItemsToStartShowingFrom..endOfItemsToShowTo]:
+    setCursorPos(2, (index + 1))
+    if index == selLocation - 1:
+      echo "\e[1;32m" & val.item & "\e[00m"
+    else:
       echo val.item
-      # if len(val.item) < maxWidthOfItem - 1:
-      #   echo val.item # & " " & $val.score
-      # else:
-      #   echo val.item[0 ..^ (maxWidthOfItem - 1)]
+    # echo val.item[0 .. min(val.item.len - 1, 160)]
 
   setCursorPos(0,0)
   echo prompt
 
-  if oldAnswer != answer:
-    oldAnswer = answer
-    # Clear answer
-    setCursorPos(len(prompt) + 1,0)
-    var widthToClear = w - (len(prompt) + 1)
-    echo " ".repeat(widthToClear)
+  # if oldAnswer != answer:
+    # oldAnswer = answer
+  # Clear answer
+  setCursorPos(len(prompt) + 1,0)
+  var widthToClear = w - (len(prompt) + 1)
+  echo " ".repeat(widthToClear)
     
 
   setCursorPos(len(prompt) + 1,0)
@@ -187,25 +211,22 @@ proc drawPromptItemsAndSelector(prompt: string, answer: string, itemsToSearch: s
     echo "\e[1;34m" & answer & "\e[00m"
   else:
     echo "\e[1;34m" & answer[0..maxAnswerWidth - 1] & "\e[00m"
-
-
-
-  var selLocation = sel
-  if sel + 1 > shownListBottom - 2:
-    selLocation = shownListBottom - 2
-  else:
-    selLocation += 1
-
-  if oldSelLocation != selLocation:
-    setCursorPos(0, oldSelLocation)
-    echo " "
-    oldSelLocation = selLocation    
   
   setCursorPos(0, selLocation)
   echo "\e[1;36m*\e[00m"
 
 proc selectFromList*(prompt: string, items: seq): int =
-  var itemsSearched  = fuzzySearchItems("", items)
+  var shortenedItems: seq = @[]
+
+  for i in items:
+    var newItem = ""
+    var strIndex = 0
+    while strIndex < i.len and strIndex < (w - 4):
+      newItem &= i[strIndex]
+      strIndex += 1
+    shortenedItems.add(newItem)
+
+  var itemsSearched  = fuzzySearchItems(0, "", shortenedItems)
 
   if getFileInfo(stdin).id.file != 37:
     var stdindup = dup(c_fileno(stdin))
@@ -220,6 +241,7 @@ proc selectFromList*(prompt: string, items: seq): int =
   
   hideCursor()
 
+  itemsSearched  = fuzzySearchItems(sel, "", shortenedItems)
   var _ = drawPromptItemsAndSelector(prompt, answer, itemsSearched, sel)
 
   result = 0
@@ -252,7 +274,7 @@ proc selectFromList*(prompt: string, items: seq): int =
         newsel -= 1
     elif ch == 66 and controlKey == 2:
       controlKey = 0
-      if sel < len(items) - 1:
+      if sel < len(shortenedItems) - 1:
         newsel += 1
     elif int(ch) == 127:
       # Backspace was hit
@@ -262,11 +284,11 @@ proc selectFromList*(prompt: string, items: seq): int =
       elif len(answer) > 1:
         newLength = len(answer) - 2
         answer = answer[0..newLength]
-      itemsSearched  = fuzzySearchItems(answer, items)
+      itemsSearched  = fuzzySearchItems(newsel, answer, shortenedItems)
       var _ = drawPromptItemsAndSelector(prompt, answer, itemsSearched, sel)
     elif isprint(ch) == true and controlKey == 0:
       answer &= char(ch)
-      itemsSearched  = fuzzySearchItems(answer, items)
+      itemsSearched  = fuzzySearchItems(newsel, answer, shortenedItems)
       var _ = drawPromptItemsAndSelector(prompt, answer, itemsSearched, sel)
       # Must check sel again here because the list of items has now changed due to the answer fzy search
     if sel > len(itemsSearched) - 1:
@@ -281,6 +303,7 @@ proc selectFromList*(prompt: string, items: seq): int =
       # setCursorPos(0, (newsel + 1))
       # echo "*"  
       sel = newsel
+      itemsSearched  = fuzzySearchItems(newsel, answer, shortenedItems)
       var selection = drawPromptItemsAndSelector(prompt, answer, itemsSearched, sel)
       if selection != -1:
         result = selection
